975d802d15269d49dd627a20bc310ce21dfb840f
Vladislav Pestsov <pestsov.js@gmail.com>
2024-05-31 22:29:50 +0300

    init commit

---
+++ cppsrc/main.cpp
@@ -0,0 +1,9 @@
+#include <napi.h>
+#include "Samples/functionexample.h"
+
+
+Napi::Object InitAll(Napi::Env env, Napi::Object exports) {
+    return functionexample::Init(env, exports);
+}
+
+NODE_API_MODULE(testaddon, InitAll)
